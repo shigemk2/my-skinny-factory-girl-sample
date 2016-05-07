fd53cb2764e7295439f33b7b9234013fb85cad97
shigemk2 <i.am.shige@gmail.com>
2016-05-07 17:17:04 +0900

    Delete newInstance

--- src/test/scala/Connection.scala
+++ src/test/scala/Connection.scala
@@ -1,6 +1,6 @@
 import scalikejdbc.ConnectionPool
 
 trait Connection {
-  Class.forName("com.mysql.jdbc.Driver").newInstance()
+  Class.forName("com.mysql.jdbc.Driver")
   ConnectionPool.add('default, "jdbc:mysql://192.168.99.100/sys", "root", "")
 }
